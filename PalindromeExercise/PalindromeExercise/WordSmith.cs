bf4c4b1e8bc178ab40b8e0b692802ec79adecd7a
David Smith <cpmpdavidsmith@icloud.com>
2023-03-02 14:46:39 -0500

    Palindrome TDD ex2

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsAPalindrome(string word)
+		{
+			string reverse = "";
+			for (int i = word.Length-1; i >= 0; i--)
+			{
+				reverse += word[i];
+
+			}
+			return word.ToLower() == reverse.ToLower();
+		}
+	}
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,28 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        //[Fact]
+        //public void Test1()
+        //{
 
+        //}
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("hello", false)]
+        [InlineData("rotator", true)]
+        public void IsAPalindromeTest(string word, bool expected)
+        {
+            //arange
+            var test = new WordSmith();
+            //act
+            bool actual = test.IsAPalindrome(word);
+            //assert
+            Assert.Equal(expected, actual);
         }
     }
 }
